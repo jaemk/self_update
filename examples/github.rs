95dae5b00f4dd9c4642d7e6816d4ca3a81b4d487
James Kominick <james.kominick@gmail.com>
2017-07-25 22:51:34 -0400

    update example

--- examples/github.rs
+++ examples/github.rs
@@ -2,28 +2,30 @@
 Example updating an executable to the latest version released via GitHub
 */
 
-// For the `crate_version!` macro
+// For the `cargo_crate_version!` macro
 #[macro_use] extern crate self_update;
 
 
 fn run() -> Result<(), Box<::std::error::Error>> {
     let target = self_update::get_target()?;
-    self_update::backends::github::Updater::configure()?
+    let status = self_update::backends::github::Updater::configure()?
         .repo_owner("jaemk")
         .repo_name("self_update")
         .target(&target)
         .bin_name("self_update_example")
-        .show_progress(true)
-        .current_version(crate_version!())
+        .show_download_progress(true)
+        //.show_output(false)
+        //.no_confirm(true)
+        .current_version(cargo_crate_version!())
         .build()?
         .update()?;
+    println!("Update status: `v{}`!", status.version());
     Ok(())
 }
 
 pub fn main() {
     if let Err(e) = run() {
-        eprintln!("[ERROR] {}", e);
+        println!("[ERROR] {}", e);
         ::std::process::exit(1);
     }
-    println!("self_update example version: `{}`!", crate_version!());
 }
