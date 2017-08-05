f72893994a03c0a3508d0a55fba22e44e37f914f
James Kominick <james.kominick@gmail.com>
2017-08-04 23:01:23 -0400

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,8 @@
 `self_update` provides updaters for updating rust executables in-place from various release
 distribution backends.
 
-Currently only GitHub releases are supported.
-
 ```shell
-self_update = "0.2"
+self_update = "0.3"
 ```
 
 ## Usage
@@ -40,8 +38,51 @@ fn update() -> Result<(), Box<::std::error::Error>> {
 # fn main() { }
 ```
 
-
 Run the above example to see `self_update` in action: `cargo run --example github`
+
+Separate utilities are also exposed:
+
+```
+extern crate self_update;
+
+fn update() -> Result<(), Box<::std::error::Error>> {
+    let target = self_update::get_target()?;
+    let releases = self_update::backends::github::ReleaseList::configure()
+        .repo_owner("jaemk")
+        .repo_name("self_update")
+        .with_target(&target)
+        .build()?
+        .fetch()?;
+    println!("found releases:");
+    println!("{:#?}\n", releases);
+
+    // get the first available release
+    let asset = releases[0]
+        .asset_for(&target).unwrap();
+
+    let tmp_dir = self_update::TempDir::new_in(::std::env::current_dir()?, "self_update")?;
+    let tmp_tarball_path = tmp_dir.path().join(&asset.name);
+    let tmp_tarball = ::std::fs::File::open(&tmp_tarball_path)?;
+
+    self_update::Download::from_url(&asset.download_url)
+        .download_to(&tmp_tarball)?;
+
+    self_update::Extract::from_source(&tmp_tarball_path)
+        .archive(self_update::ArchiveKind::Tar)
+        .encoding(self_update::EncodingKind::Gz)
+        .extract_into(&tmp_dir.path())?;
+
+    let tmp_file = tmp_dir.path().join("replacement_tmp");
+    let bin_name = "self_update_bin";
+    let bin_path = tmp_dir.path().join(bin_name);
+    self_update::Move::from_source(&bin_path)
+        .replace_using_temp(&tmp_file)
+        .to_dest(&::std::env::current_exe()?)?;
+
+    Ok(())
+}
+# fn main() { }
+```
 
 */
 extern crate serde_json;
