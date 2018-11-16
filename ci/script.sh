1b42f16e3c2023ff10466ed89590b64ead4b53b6
James Kominick <james@kominick.com>
2018-11-15 23:52:53 -0500

    respect DISABLE_TESTS

--- ci/script.sh
+++ ci/script.sh
@@ -7,9 +7,9 @@ main() {
     #cross build --target $TARGET
     #cross build --target $TARGET --release
 
-    #if [ ! -z $DISABLE_TESTS ]; then
-    #    return
-    #fi
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
 
     #cross test --target $TARGET
     cross test --target $TARGET --release
