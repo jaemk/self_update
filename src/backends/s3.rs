ec33cda0a85c3a1220c4ee580d4101fe70144320
James Kominick <james@kominick.com>
2020-01-15 07:03:19 -0500

    make sure version numbers are stripped of prefixes to prevent parsing errors

--- src/backends/s3.rs
+++ src/backends/s3.rs
@@ -516,7 +516,8 @@ fn fetch_releases_from_s3(
                             if let Some(captures) = regex.captures(&txt) {
                                 let release = current_release.get_or_insert(Release::default());
                                 release.name = captures["name"].to_string();
-                                release.version = captures["version"].to_string();
+                                release.version =
+                                    captures["version"].trim_start_matches("v").to_string();
                                 release.assets = vec![ReleaseAsset {
                                     name: txt.clone(),
                                     download_url: format!("{}{}", download_base_url, txt),
