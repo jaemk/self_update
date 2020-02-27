b96448dbdb59cf0fb18bea0224de35810a74b959
Roey Darwish Dror <roey.ghost@gmail.com>
2020-02-27 11:07:01 +0200

    Remove formatting

--- src/backends/github.rs
+++ src/backends/github.rs
@@ -551,7 +551,9 @@ fn api_headers(auth_token: &Option<String>) -> Result<header::HeaderMap> {
     let mut headers = header::HeaderMap::new();
     headers.insert(
         header::USER_AGENT,
-        "rust-reqwest/self-update".parse().expect("github invalid user-agent"),
+        "rust-reqwest/self-update"
+            .parse()
+            .expect("github invalid user-agent"),
     );
 
     if let Some(token) = auth_token {
