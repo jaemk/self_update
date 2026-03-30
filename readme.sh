8b50a3f69d16237d1926ec3945b7a0491317fa3c
James Kominick <james@kominick.com>
2026-03-30 13:15:24 -0400

    add gh-action build workflow

--- readme.sh
+++ readme.sh
@@ -1,1 +1,13 @@
-cargo readme --no-indent-headings > README.md
+
+CHECK="${1:-}"
+if [[ "$CHECK" == "check" ]]; then
+    echo "checking README.md..."
+	cargo readme --no-indent-headings > _tmp_readme.md
+	cmp README.md _tmp_readme.md
+    rc=$?
+	rm -f _tmp_readme.md
+    exit $rc
+else
+    echo "generating README.md..."
+    cargo readme --no-indent-headings > README.md
+fi
